b292a88f825747f763613ff2e14a351a94a1b51a
Parth Patel <parthpatel@Parths-MacBook-Pro.local>
2023-06-11 20:00:40 -0400

    building automl with spark

--- spark/spark_init.py
+++ spark/spark_init.py
@@ -15,13 +15,11 @@ class SparkInitialization(ABC):
             return json.load(conf)
         
     def _init_spark(self, extra_conf = None):
-
         #will replace this default config to set default while submiting spark 
         spark =  spark = SparkSession.builder \
                             .master("local[*]") \
                             .enableHiveSupport() \
                             .appName(self.spark_conf.get("applicationName")) \
-                            .config("spark.sql.warehouse.dir","/Users/parthpatel/Applications/Databricks_app") \
                             .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
                             .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
                             .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
@@ -32,6 +30,6 @@ class SparkInitialization(ABC):
                     spark.conf.set(key, value)
 
         return spark
-        
+         
 
 
